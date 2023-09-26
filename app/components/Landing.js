7ed5cfd66088dc7e99d209da6dd54bd9412d8adf
Abdulloh <abdujabborov6172@gmail.com>
2023-09-27 00:40:17 +0500

    A

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -44,90 +44,15 @@ const Landing = () => {
       eval(code); 
 
       setConsoleOutput(consoleMessages);
+      showSuccessToast()
     } catch (error) {
+      const consoleMessages = []
+      consoleMessages.push(`Error: ${error.message}`);
       console.error(error);
+      setConsoleOutput(consoleMessages);
+      showErrorToast(error.message)
     }
   };
-  
-  // const handleCompile = () => {
-  //   setProcessing(true);
-  //   const formData = {
-  //     language_id: 63,
-  //     // encode source code in base64
-  //     source_code: btoa(code),
-  //   };
-  //   const options = {
-  //     method: "POST",
-  //     url: process.env.REACT_APP_RAPID_API_URL,
-  //     params: { base64_encoded: "true", fields: "*" },
-  //     headers: {
-  //       "content-type": "application/json",
-  //       "Content-Type": "application/json",
-  //       "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-  //       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-  //     },
-  //     data: formData,
-  //   };
-
-  //   axios
-  //     .request(options)
-  //     .then(function (response) {
-  //       console.log("res.data", response.data);
-  //       const token = response.data.token;
-  //       checkStatus(token);
-  //     })
-  //     .catch((err) => {
-  //       let error = err.response ? err.response.data : err;
-  //       // get error status
-  //       let status = err.response.status;
-  //       console.log("status", status);
-  //       if (status === 429) {
-  //         console.log("too many requests", status);
-
-  //         showErrorToast(
-  //           `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
-  //           10000
-  //         );
-  //       }
-  //       setProcessing(false);
-  //       console.log("catch block...", error);
-  //     });
-  // };
-
-  // const checkStatus = async (token) => {
-  //   const options = {
-  //     method: "GET",
-  //     url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
-  //     params: { base64_encoded: "true", fields: "*" },
-  //     headers: {
-  //       "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-  //       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-  //     },
-  //   };
-  //   try {
-  //     let response = await axios.request(options);
-  //     let statusId = response.data.status?.id;
-
-  //     // Processed - we have a result
-  //     if (statusId === 1 || statusId === 2) {
-  //       // still processing
-  //       setTimeout(() => {
-  //         checkStatus(token);
-  //       }, 2000);
-  //       return;
-  //     } else {
-  //       setProcessing(false);
-  //       setOutputDetails(response.data);
-  //       showSuccessToast(`Compiled Successfully!`);
-  //       console.log("response.data", response.data);
-  //       return;
-  //     }
-  //   } catch (err) {
-  //     console.log("err", err);
-  //     setProcessing(false);
-  //     showErrorToast();
-  //   }
-  // };
 
   function handleThemeChange(th) {
     const theme = th;
@@ -146,29 +71,29 @@ const Landing = () => {
     );
   }, []);
 
-  // const showSuccessToast = (msg) => {
-  //   toast.success(msg || `Compiled Successfully!`, {
-  //     position: "top-right",
-  //     autoClose: 1000,
-  //     hideProgressBar: false,
-  //     closeOnClick: true,
-  //     pauseOnHover: true,
-  //     draggable: true,
-  //     progress: undefined,
-  //   });
-  // };
+  const showSuccessToast = (msg) => {
+    toast.success(msg || `Compiled Successfully!`, {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
-  // const showErrorToast = (msg, timer) => {
-  //   toast.error(msg || `Something went wrong! Please try again.`, {
-  //     position: "top-right",
-  //     autoClose: timer ? timer : 1000,
-  //     hideProgressBar: false,
-  //     closeOnClick: true,
-  //     pauseOnHover: true,
-  //     draggable: true,
-  //     progress: undefined,
-  //   });
-  // };
+  const showErrorToast = (msg, timer) => {
+    toast.error(msg || `Something went wrong! Please try again.`, {
+      position: "top-right",
+      autoClose: timer ? timer : 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
   return (
     <>
