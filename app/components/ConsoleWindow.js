70a23a91727b02c762d91ed6dff92705db78675e
Abdulloh <abdujabborov6172@gmail.com>
2023-09-27 08:07:06 +0500

    A

--- app/components/ConsoleWindow.js
+++ app/components/ConsoleWindow.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const ConsoleWindow = ({ output }) => {
   return (
-    <div className="w-full h-[100%] px-5 py-2 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
-      {output.map((message, index) => (
-        <div key={index} className="text-[13px]">
-          {message}
-        </div>
-      ))}
+    <div className='flex flex-col h-[100%]'>
+      <h2 className='text-base font-semibold font-sans'>Output</h2>
+      <div className="w-full h-[100%] px-5 py-2 mt-1 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
+        {output.map((message, index) => (
+          <div key={index} className="text-[13px]">
+            {message}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
